f663e982f158afac8d74aa1675eb077595e3771c
Federico Griggio <griggio.federico@gmail.com>
2025-06-11 09:45:43 +0200

    viva 2011

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
+st.markdown("""# Hello Awesome batch 2011 🧉
 ### This is a sub sub header
 This is text""")
 
